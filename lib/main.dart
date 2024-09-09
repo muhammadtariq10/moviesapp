d04b0c8283b750efd0e8d9f61ab3297c8f341e61
muhammadtariq10 <mohamedelrawesh50@gmail.com>
2024-09-09 16:18:09 +0300

    finish splash screen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:movies/home_screen.dart';
+
+void main() {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (_) => const HomeScreen(),
+      },
+    );
+  }
+}
