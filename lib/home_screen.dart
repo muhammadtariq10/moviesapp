b6f7e0c6f41555ebd06dea03ed2ee8d94adb1798
Ayat Esaam <eayatessam1@gmail.com>
2024-09-17 21:20:27 +0300

    impelemnt

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,66 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:movies/bottomnavigationbar/browse/browes_tap.dart';
-import 'package:movies/bottomnavigationbar/home/home_tap.dart';
-import 'package:movies/bottomnavigationbar/search_tap.dart';
-import 'package:movies/bottomnavigationbar/watch_list_tap.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   static const String routeName = '/';
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int currentIndex = 0;
-  List<Widget> taps = [
-    HomeTap(),
-    SearchTap(),
-    BrowseTab(),
-    WatchListTap(),
-  ];
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: taps[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          currentIndex = index;
-          setState(() {});
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              size: 32,
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-              size: 32,
-            ),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/icon material-movie.png'),
-              size: 32,
-            ),
-            label: 'Browse',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/icon ionic-md-bookmarks.png'),
-              size: 32,
-            ),
-            label: 'Watch List',
-          ),
-        ],
-      ),
-    );
+    return const Scaffold();
   }
 }
